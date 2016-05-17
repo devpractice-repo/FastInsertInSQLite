1b830d008527197861da544886aa87fdcca4da7c
Marat <marat-ab@mail.ru>
2016-05-17 22:40:17 +0500

    [create repository]

---
+++ SQLiteFastInsert/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SQLiteFastInsert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbTimeOfExper = new System.Windows.Forms.Label();
+            this.tbTimeOfExperiment = new System.Windows.Forms.TextBox();
+            this.lbCIIT = new System.Windows.Forms.Label();
+            this.tbInsCount = new System.Windows.Forms.TextBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.lbInsPerSec = new System.Windows.Forms.Label();
+            this.lbInsPerSecValue = new System.Windows.Forms.Label();
+            this.timerDb = new System.Windows.Forms.Timer(this.components);
+            this.lbCountOfInserts = new System.Windows.Forms.Label();
+            this.lbCountOfInsertsValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbTimeOfExper
+            // 
+            this.lbTimeOfExper.AutoSize = true;
+            this.lbTimeOfExper.Location = new System.Drawing.Point(42, 15);
+            this.lbTimeOfExper.Name = "lbTimeOfExper";
+            this.lbTimeOfExper.Size = new System.Drawing.Size(119, 13);
+            this.lbTimeOfExper.TabIndex = 0;
+            this.lbTimeOfExper.Text = "Time of experiment, sec";
+            // 
+            // tbTimeOfExperiment
+            // 
+            this.tbTimeOfExperiment.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbTimeOfExperiment.Location = new System.Drawing.Point(167, 11);
+            this.tbTimeOfExperiment.Name = "tbTimeOfExperiment";
+            this.tbTimeOfExperiment.Size = new System.Drawing.Size(100, 20);
+            this.tbTimeOfExperiment.TabIndex = 1;
+            // 
+            // lbCIIT
+            // 
+            this.lbCIIT.AutoSize = true;
+            this.lbCIIT.Location = new System.Drawing.Point(12, 49);
+            this.lbCIIT.Name = "lbCIIT";
+            this.lbCIIT.Size = new System.Drawing.Size(149, 13);
+            this.lbCIIT.TabIndex = 2;
+            this.lbCIIT.Text = "Count INSERTs in transaction";
+            // 
+            // tbInsCount
+            // 
+            this.tbInsCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbInsCount.Location = new System.Drawing.Point(167, 45);
+            this.tbInsCount.Name = "tbInsCount";
+            this.tbInsCount.Size = new System.Drawing.Size(100, 20);
+            this.tbInsCount.TabIndex = 3;
+            // 
+            // btStart
+            // 
+            this.btStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btStart.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btStart.Location = new System.Drawing.Point(15, 81);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "START";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btStop.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btStop.Location = new System.Drawing.Point(96, 81);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(75, 23);
+            this.btStop.TabIndex = 5;
+            this.btStop.Text = "STOP";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // lbInsPerSec
+            // 
+            this.lbInsPerSec.AutoSize = true;
+            this.lbInsPerSec.Location = new System.Drawing.Point(12, 125);
+            this.lbInsPerSec.Name = "lbInsPerSec";
+            this.lbInsPerSec.Size = new System.Drawing.Size(111, 13);
+            this.lbInsPerSec.TabIndex = 6;
+            this.lbInsPerSec.Text = "INSERTs per second:";
+            // 
+            // lbInsPerSecValue
+            // 
+            this.lbInsPerSecValue.AutoSize = true;
+            this.lbInsPerSecValue.Location = new System.Drawing.Point(124, 125);
+            this.lbInsPerSecValue.Name = "lbInsPerSecValue";
+            this.lbInsPerSecValue.Size = new System.Drawing.Size(28, 13);
+            this.lbInsPerSecValue.TabIndex = 7;
+            this.lbInsPerSecValue.Text = "-------";
+            // 
+            // timerDb
+            // 
+            this.timerDb.Tick += new System.EventHandler(this.timerDb_Tick);
+            // 
+            // lbCountOfInserts
+            // 
+            this.lbCountOfInserts.AutoSize = true;
+            this.lbCountOfInserts.Location = new System.Drawing.Point(12, 150);
+            this.lbCountOfInserts.Name = "lbCountOfInserts";
+            this.lbCountOfInserts.Size = new System.Drawing.Size(98, 13);
+            this.lbCountOfInserts.TabIndex = 8;
+            this.lbCountOfInserts.Text = "Count of INSERTs:";
+            // 
+            // lbCountOfInsertsValue
+            // 
+            this.lbCountOfInsertsValue.AutoSize = true;
+            this.lbCountOfInsertsValue.Location = new System.Drawing.Point(124, 150);
+            this.lbCountOfInsertsValue.Name = "lbCountOfInsertsValue";
+            this.lbCountOfInsertsValue.Size = new System.Drawing.Size(28, 13);
+            this.lbCountOfInsertsValue.TabIndex = 9;
+            this.lbCountOfInsertsValue.Text = "-------";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 181);
+            this.Controls.Add(this.lbCountOfInsertsValue);
+            this.Controls.Add(this.lbCountOfInserts);
+            this.Controls.Add(this.lbInsPerSecValue);
+            this.Controls.Add(this.lbInsPerSec);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.tbInsCount);
+            this.Controls.Add(this.lbCIIT);
+            this.Controls.Add(this.tbTimeOfExperiment);
+            this.Controls.Add(this.lbTimeOfExper);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "SQLite Fast INSERT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTimeOfExper;
+        private System.Windows.Forms.TextBox tbTimeOfExperiment;
+        private System.Windows.Forms.Label lbCIIT;
+        private System.Windows.Forms.TextBox tbInsCount;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Label lbInsPerSec;
+        private System.Windows.Forms.Label lbInsPerSecValue;
+        private System.Windows.Forms.Timer timerDb;
+        private System.Windows.Forms.Label lbCountOfInserts;
+        private System.Windows.Forms.Label lbCountOfInsertsValue;
+    }
+}
+
---
+++ SQLiteFastInsert/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+using System.Data.SQLite;
+using System.IO;
+
+
+namespace SQLiteFastInsert
+{
+    public partial class Form1 : Form
+    {        
+        private int intervalOfTimer;
+        private int countOfInsertsInTrns;
+        private int insertsCounter;
+
+        // Vars for work with DB
+        private String dbFileName;
+        private SQLiteConnection m_dbConn;
+        private SQLiteCommand m_sqlCmd;
+        private String[] insertsPoll;
+
+        // Vars for thread
+        private Thread threadDbWorker;
+        private bool isThreadRun;
+        private bool stopThread;
+                
+
+        public Form1()
+        {
+            InitializeComponent();                      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Interface tuning
+            tbTimeOfExperiment.Text = intervalOfTimer.ToString();
+            tbInsCount.Text = countOfInsertsInTrns.ToString();
+            btStop.Enabled = false;
+
+            // Init vars
+            intervalOfTimer = 0;
+            countOfInsertsInTrns = 0;
+            insertsCounter = 0;
+            isThreadRun = false;
+            stopThread = true;
+            
+            // Prepare DB vars for work
+            m_dbConn = new SQLiteConnection();
+            m_sqlCmd = new SQLiteCommand();
+
+            dbFileName = "sample.sqlite";
+
+            if (!File.Exists(dbFileName))
+                SQLiteConnection.CreateFile(dbFileName);
+
+            try
+            {
+                m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                m_dbConn.Open();
+                m_sqlCmd.Connection = m_dbConn;
+
+                m_sqlCmd.CommandText = "CREATE TABLE IF NOT EXISTS Test (id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT)";
+                m_sqlCmd.ExecuteNonQuery();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {    
+            // Time of experiment and INSERTs in transaction        
+            if(Int32.TryParse(tbTimeOfExperiment.Text, out intervalOfTimer))
+            {
+                if(intervalOfTimer <= 0)
+                {
+                    MessageBox.Show("Time of experiment must be higher then zero!");
+                    return;
+                }
+            }
+
+            if(Int32.TryParse(tbInsCount.Text, out countOfInsertsInTrns))
+            {
+                if(countOfInsertsInTrns <= 0)
+                {
+                    MessageBox.Show("Count of INSERTs must be higher then zero!");
+                    return;
+                }
+            }
+
+            // Run experiment
+            DbWorker();
+            
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            // Stop experiment
+            DbWorker();
+        }
+
+        private void DbWorker()
+        {
+            if(isThreadRun)
+            {
+                // Prep interface
+                btStart.Enabled = true;
+                btStop.Enabled = false;
+
+                // Set flags
+                stopThread = true;
+                isThreadRun = false;
+
+                // Timer off
+                timerDb.Enabled = false;                
+            }
+            else
+            {
+                // Prep interface
+                btStart.Enabled = false;
+                btStop.Enabled = true;
+                lbCountOfInsertsValue.Text = "-------";
+                lbInsPerSecValue.Text = "-------";
+
+                // Set flags
+                isThreadRun = true;
+                stopThread = false;
+
+                // Run thread
+                threadDbWorker = new Thread(writeDataToDb);
+                threadDbWorker.Start();
+
+                // Timer on
+                timerDb.Interval = intervalOfTimer * 1000;
+                timerDb.Enabled = true;                
+            }
+        }
+
+            private void writeDataToDb()
+            {            
+                String query = "INSERT INTO Test ('message') values ('Hello message')";
+                while (!stopThread)
+                {
+                    if (countOfInsertsInTrns == 1)
+                    {
+                        m_sqlCmd = new SQLiteCommand(query, m_sqlCmd.Connection);
+                        m_sqlCmd.ExecuteNonQuery();
+                        insertsCounter++;
+                    }
+                    else
+                    {
+                        m_sqlCmd = new SQLiteCommand("begin", m_sqlCmd.Connection);
+                        m_sqlCmd.ExecuteNonQuery();
+                        //---INSIDE LOOP
+                        for (int i = 0; i < countOfInsertsInTrns; i++)
+                        {
+                            m_sqlCmd = new SQLiteCommand(query, m_sqlCmd.Connection);
+                            m_sqlCmd.ExecuteNonQuery();
+                            insertsCounter++;
+                        }
+                        //---END LOOP
+                        m_sqlCmd = new SQLiteCommand("end", m_sqlCmd.Connection);
+                        m_sqlCmd.ExecuteNonQuery();
+                    }
+                }
+            }
+
+       
+
+        private void timerDb_Tick(object sender, EventArgs e)
+        {
+            // Stop timer
+            timerDb.Stop();
+            timerDb.Enabled = false;
+
+            // Stop thread
+            if (isThreadRun)                         
+                stopThread = true;
+         
+            DbWorker();
+
+            // Calc metrics
+            float avrg = ((float)insertsCounter) / intervalOfTimer;
+            lbCountOfInsertsValue.Text = insertsCounter.ToString();
+            lbInsPerSecValue.Text = avrg.ToString();
+        }
+    }
+}
